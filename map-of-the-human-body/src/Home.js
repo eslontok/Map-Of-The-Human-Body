7210928eb972ddc7497f7f7ecf7b74fd56d84b05
eslontok <146041985+eslontok@users.noreply.github.com>
2024-11-05 07:01:03 -0500

    Rough Outline - Home Page

---
+++ map-of-the-human-body/src/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div className="home">
+            <h2>Welcome to the Map Of The Human Body!</h2>
+        </div>
+    );
+}
+
+export default Home;
---
+++ map-of-the-human-body/src/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Map</a>
+                <a href="/">Systems</a>
+                <a href="/">Diseases</a>
+                <a href="/">Treatments</a>
+                <a href="/">About</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
