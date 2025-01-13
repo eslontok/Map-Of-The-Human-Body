497a4793756abe062a957596579317c0d85b667b
eslontok <146041985+eslontok@users.noreply.github.com>
2025-01-13 04:50:01 -0500

    Added Delete Discussion Button

--- map-of-the-human-body/src/Discussions.js
+++ map-of-the-human-body/src/Discussions.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import DiscussionsList from "./DiscussionsList";
 
 function Discussions(){
@@ -9,10 +9,20 @@ function Discussions(){
         {id: 3, title: "Discussion3", author: "anonymous3", body: "This is discussion3 by anonymous3"}
     ]);
 
+    const handleDelete = (id) => {
+        const newDiscussions = discussions.filter(discussion => discussion.id !== id);
+        setDiscussions(newDiscussions);
+    }
+
+    useEffect(() => {
+        console.log("Use Effect Works!");
+        console.log(discussions);
+    });
+
     return(
         <div className="discussions">
             <h2>Discussions</h2>
-            <DiscussionsList discussions={discussions}/>
+            <DiscussionsList discussions={discussions} handleDelete={handleDelete}/>
         </div>
     );
 }
--- map-of-the-human-body/src/DiscussionsList.js
+++ map-of-the-human-body/src/DiscussionsList.js
@@ -1,14 +1,16 @@
 function DiscussionsList(props){
 
     const discussions = props.discussions;
+    const handleDelete = props.handleDelete;
 
     return(
         <div className="discussionsList">
             {discussions.map((discussion) => (
                 <div className="discussionPreview" key={discussion.id}>
                     <h3>{discussion.title}</h3>
-                    <p>Posted by: {discussion.author}</p>
+                    <h4>Posted by: {discussion.author}</h4>
                     <p>{discussion.body}</p>
+                    <button onClick={() => handleDelete(discussion.id)}>Delete</button>
                 </div>
             ))}
         </div>
