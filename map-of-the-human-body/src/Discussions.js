c0874dbbb515392ed3d2b7ab3c454f3d5da8d2c3
eslontok <146041985+eslontok@users.noreply.github.com>
2025-01-17 03:07:37 -0500

    Set Up JSON Server and Discussions

--- map-of-the-human-body/src/Discussions.js
+++ map-of-the-human-body/src/Discussions.js
@@ -3,11 +3,7 @@ import DiscussionsList from "./DiscussionsList";
 
 function Discussions(){
 
-    const [discussions, setDiscussions] = useState([
-        {id: 1, title: "Discussion1", author: "anonymous1", body: "This is discussion1 by anonymous1", likes: 0, dislikes: 0},
-        {id: 2, title: "Discussion2", author: "anonymous2", body: "This is discussion2 by anonymous2", likes: 0, dislikes: 0},
-        {id: 3, title: "Discussion3", author: "anonymous3", body: "This is discussion3 by anonymous3", likes: 0, dislikes: 0}
-    ]);
+    const [discussions, setDiscussions] = useState(null);
 
     const handleLike = (id) => {
         const newDiscussions = discussions.map(discussion => {
@@ -38,13 +34,19 @@ function Discussions(){
 
     useEffect(() => {
         console.log("Use Effect Triggered!");
-        //console.log(discussions);
-    });
+        fetch("http://localhost:8000/discussions")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setDiscussions(data);
+            })
+    }, []);
 
     return(
         <div className="discussions">
             <h2>Discussions</h2>
-            <DiscussionsList discussions={discussions} handleDelete={handleDelete} handleLike={handleLike} handleDislike={handleDislike}/>
+            {discussions && <DiscussionsList discussions={discussions} handleDelete={handleDelete} handleLike={handleLike} handleDislike={handleDislike}/>}
         </div>
     );
 }
