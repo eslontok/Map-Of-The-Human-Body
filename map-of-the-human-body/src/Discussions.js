2910b281cac6c4c8862c7b8e27af3c369b173d8b
eslontok <146041985+eslontok@users.noreply.github.com>
2025-01-19 04:56:38 -0500

    Catch/Throw Fetch Errors

--- map-of-the-human-body/src/Discussions.js
+++ map-of-the-human-body/src/Discussions.js
@@ -4,6 +4,8 @@ import DiscussionsList from "./DiscussionsList";
 function Discussions(){
 
     const [discussions, setDiscussions] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleLike = (id) => {
         const newDiscussions = discussions.map(discussion => {
@@ -33,19 +35,31 @@ function Discussions(){
     }
 
     useEffect(() => {
-        console.log("Use Effect Triggered!");
-        fetch("http://localhost:8000/discussions")
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                setDiscussions(data);
-            })
+        setTimeout(() => {
+            fetch("http://localhost:8000/discussions")
+                .then(response => {
+                    if(!response.ok){
+                        throw Error("ERROR: Server reached but could not fetch data!");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setDiscussions(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(error =>{
+                    setIsLoading(false);
+                    setError(error.message);
+                });
+        }, 1000);
     }, []);
 
     return(
         <div className="discussions">
             <h2>Discussions</h2>
+            {error && <div style={{marginTop: "20px", color: "#D2042D"}}>{error}</div>}
+            {isLoading && <div style={{marginTop: "20px"}}>Loading discussions...</div>}
             {discussions && <DiscussionsList discussions={discussions} handleDelete={handleDelete} handleLike={handleLike} handleDislike={handleDislike}/>}
         </div>
     );
