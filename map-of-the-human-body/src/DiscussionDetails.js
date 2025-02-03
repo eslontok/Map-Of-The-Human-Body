8145f3874420dd3c5128e571a5f1142c0ca6e692
eslontok <146041985+eslontok@users.noreply.github.com>
2025-02-03 17:53:36 -0500

    Discussion Details Page

--- map-of-the-human-body/src/DiscussionDetails.js
+++ map-of-the-human-body/src/DiscussionDetails.js
@@ -1,12 +1,22 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./useFetch";
 
 function DiscussionDetails(){
 
     const {id} = useParams();
+    const {data: discussion, isLoading, error} = useFetch("http://localhost:8000/discussions/" + id);
 
     return(
         <div className="discussionDetails">
-            <h2>Discussion details for Discussion {id}</h2>
+            {error && <div style={{marginTop: "5px", color: "#D2042D"}}>{error}</div>}
+            {isLoading && <div style={{marginTop: "5px"}}>Loading discussion...</div>}
+            {discussion && (
+                <article>
+                    <h2>{discussion.title}</h2>
+                    <h3>Posted by: {discussion.author}</h3>
+                    <p>{discussion.body}</p>
+                </article>
+            )}
         </div>
     );
 }
