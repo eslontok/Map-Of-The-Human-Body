452baaf230cea825a1a2ca86b8c192ed91839f66
eslontok <146041985+eslontok@users.noreply.github.com>
2025-02-15 05:05:48 -0500

    Edit Input Forms

--- map-of-the-human-body/src/Edit.js
+++ map-of-the-human-body/src/Edit.js
@@ -1,18 +1,31 @@
 import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
+import useFetch from "./useFetch";
 
 function Edit(){
+
+    const {id} = useParams();
+    const {data: discussion, isLoading, error} = useFetch("http://localhost:8000/discussions/" + id);
+
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [body, setBody] = useState("");
+
     return(
         <div className="edit">
             <h2>Edit Discussion</h2>
-            <form>
-                <label>Title:</label>
-                <input type="text" required/>
-                <label>Author:</label>
-                <input type="text" required/>
-                <textarea required></textarea>
-                <button>Upload</button>
-            </form>
+            {error && <div style={{marginTop: "5px", color: "#D2042D"}}>{error}</div>}
+            {isLoading && <div style={{marginTop: "5px"}}>Loading discussion...</div>}
+            {discussion && (
+                <form>
+                    <label>Title:</label>
+                    <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <label>Author:</label>
+                    <input type="text" required value={author} onChange={(e) => setAuthor(e.target.value)}/>
+                    <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                    <button>Upload</button>
+                </form>
+            )}
         </div>
     );
 }
