2fdd0012bc6ceae830808cc0ef19442935fc1826
eslontok <146041985+eslontok@users.noreply.github.com>
2025-02-21 16:37:03 -0500

    Functional Like and Dislike

--- map-of-the-human-body/src/Discussions.js
+++ map-of-the-human-body/src/Discussions.js
@@ -8,22 +8,20 @@ function Discussions(){
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const handleLike = (id) => {
+    const updateLikeDislike = (id, like, dislike) => {
         const newDiscussions = discussions.map(discussion => {
             const newDiscussion = {...discussion};
             if(newDiscussion.id === id){
-                newDiscussion.likes++;
-            }
-            return newDiscussion;
-        });
-        setDiscussions(newDiscussions);
-    }
-
-    const handleDislike = (id) => {
-        const newDiscussions = discussions.map(discussion => {
-            const newDiscussion = {...discussion};
-            if(newDiscussion.id === id){
-                newDiscussion.dislikes++;
+                newDiscussion.likes += like;
+                newDiscussion.dislikes += dislike;
+                fetch("http://localhost:8000/discussions/" + id, {
+                    method: "PATCH",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({
+                        likes: newDiscussion.likes,
+                        dislikes: newDiscussion.dislikes
+                    })
+                });
             }
             return newDiscussion;
         });
@@ -63,7 +61,7 @@ function Discussions(){
             </Link>
             {error && <div style={{marginTop: "20px", color: "#D2042D"}}>{error}</div>}
             {isLoading && <div style={{marginTop: "20px"}}>Loading discussions...</div>}
-            {discussions && <DiscussionsList discussions={discussions} handleLike={handleLike} handleDislike={handleDislike}/>}
+            {discussions && <DiscussionsList discussions={discussions} updateLikeDislike={updateLikeDislike}/>}
         </div>
     );
 }
--- map-of-the-human-body/src/DiscussionsList.js
+++ map-of-the-human-body/src/DiscussionsList.js
@@ -1,10 +1,58 @@
 import {Link} from "react-router-dom";
+import map from "./LikeDislikeMap";
 
 function DiscussionsList(props){
 
     const discussions = props.discussions;
-    const handleLike = props.handleLike;
-    const handleDislike = props.handleDislike;
+    const updateLikeDislike = props.updateLikeDislike;
+
+    const handleLike = (id) => { //map.get(id) = [liked?, disliked?] for discussion.id === id
+        let like = 0;
+        let dislike = 0;
+        if(map.has(id)){
+            const arr = map.get(id);
+            const liked = arr[0];
+            const disliked = arr[1];
+            if(liked){
+                map.set(id, [false, false]);
+                like = -1;
+            }else{
+                map.set(id, [true, false]);
+                like = 1;
+            }
+            if(disliked){
+                dislike = -1;
+            }
+        }else{
+            map.set(id, [true, false]);
+            like = 1;
+        }
+        updateLikeDislike(id, like, dislike);
+    }
+
+    const handleDislike = (id) => { //map.get(id) = [liked?, disliked?] for discussion.id === id
+        let like = 0;
+        let dislike = 0;
+        if(map.has(id)){
+            const arr = map.get(id);
+            const liked = arr[0];
+            const disliked = arr[1];
+            if(disliked){
+                map.set(id, [false, false]);
+                dislike = -1;
+            }else{
+                map.set(id, [false, true]);
+                dislike = 1;
+            }
+            if(liked){
+                like = -1;
+            }
+        }else{
+            map.set(id, [false, true]);
+            dislike = 1;
+        }
+        updateLikeDislike(id, like, dislike);
+    }
 
     return(
         <div className="discussionsList">
@@ -16,11 +64,13 @@ function DiscussionsList(props){
                         <p>{discussion.body}</p>
                     </Link>
                     <button onClick={() => handleLike(discussion.id)}>
-                        <span className="material-symbols-outlined">thumb_up</span>
+                        {(map.has(discussion.id) && map.get(discussion.id)[0] === true) && <span className="material-symbols-outlined" style={{color: "#00FFFF"}}>thumb_up</span>}
+                        {(!map.has(discussion.id) || map.get(discussion.id)[0] === false) && <span className="material-symbols-outlined">thumb_up</span>}
                         {discussion.likes}
                     </button>
                     <button onClick={() => handleDislike(discussion.id)}>
-                        <span className="material-symbols-outlined">thumb_down</span>
+                        {(map.has(discussion.id) && map.get(discussion.id)[1] === true) && <span className="material-symbols-outlined" style={{color: "#00FFFF"}}>thumb_down</span>}
+                        {(!map.has(discussion.id) || map.get(discussion.id)[1] === false) && <span className="material-symbols-outlined">thumb_down</span>}
                         {discussion.dislikes}
                     </button>
                     <button>
