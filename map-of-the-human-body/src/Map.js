3349ed3a4ec87222fe5299fa8f44f79f6811c624
eslontok <146041985+eslontok@users.noreply.github.com>
2025-03-21 03:02:15 -0400

    Initialize Map Page

--- map-of-the-human-body/src/Map.js
+++ map-of-the-human-body/src/Map.js
@@ -1,12 +1,28 @@
 import "./css/map.css";
+import {useState} from "react";
 import ScrollToTop from "./ScrollToTop";
 
 function Map(){
+
+    const [selection, setSelection] = useState("Organs");
+
+    const handleChange = (e) => {
+        setSelection(e.target.value);
+    }
+
     return(
-        <div className="map">
-            <ScrollToTop/>
-            <h2>Map Page</h2>
-            <p>Coming Soon!</p>
+        <div className="mapScene">
+            <div className="map">
+                <ScrollToTop/>
+                <h2>Navigate The Body</h2>
+                <label>Map Of:</label>
+                <select value={selection} onChange={(e) => handleChange(e)}>
+                    <option value="Organs">Organs</option>
+                    <option value="Skeleton">Skeleton</option>
+                    <option value="Muscles (Front)">Muscles (Front)</option>
+                    <option value="Muscles (Back)">Muscles (Back)</option>
+                </select>
+            </div>
         </div>
     );
 }
