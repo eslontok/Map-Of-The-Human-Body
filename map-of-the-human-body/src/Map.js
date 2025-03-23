d81e400e146e6d910b15c6f6b05d6c23eac122bc
eslontok <146041985+eslontok@users.noreply.github.com>
2025-03-23 01:27:34 -0400

    Reset Map Layout

--- map-of-the-human-body/src/Map.js
+++ map-of-the-human-body/src/Map.js
@@ -9,7 +9,7 @@ function Map(){
     const organsParts = ["All","Brain","Esophagus","Lungs","Heart","Stomach","Liver","Pancreas","Gallbladder","Small Intestine","Large Intestine","Kidney","Bladder"];
     const skeletonParts = ["All","Head","Collar","Upper Arm","Forearm","Hands","Spine","Torso","Hip","Thigh","Lower Leg","Feet"];
     const musclesFrontParts = ["All","Head","Shoulders","Upper Arm","Forearm","Hands","Chest","Abdomen","Obliques","Thigh","Lower Leg","Feet"];
-    const musclesBackParts = ["All","Traps","Shoulders","Upper Arm","Lats","Obliques","Glutes","Thigh","Lower Leg"];
+    const musclesBackParts = ["All","Traps","Shoulders","Upper Arm","Lats","Obliques","Lower Back","Glutes","Thigh","Lower Leg"];
 
     const handleChange = (e) => {
         setSelection(e.target.value);
@@ -28,34 +28,7 @@ function Map(){
                     <option value="Muscles (Back)">Muscles (Back)</option>
                 </select>
                 <div className="mapDetails">
-                    <div className="mapParts">
-                        {selection === "Organs" && organsParts.map((organ) => (
-                            <div key={organ}>
-                                <button value={organ}>{organ}</button>
-                            </div>
-                        ))}
-                        {selection === "Skeleton" && skeletonParts.map((bone) => (
-                            <div key={bone}>
-                                <button value={bone}>{bone}</button>
-                            </div>
-                        ))}
-                        {selection === "Muscles (Front)" && musclesFrontParts.map((muscle) => (
-                            <div key={muscle}>
-                                <button value={muscle}>{muscle}</button>
-                            </div>
-                        ))}
-                        {selection === "Muscles (Back)" && musclesBackParts.map((muscle) => (
-                            <div key={muscle}>
-                                <button value={muscle}>{muscle}</button>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="mapDiagram">
-                        <button>Image Goes Here</button>
-                    </div>
-                    <div className="mapDescription">
-                        <p>This is random content that will be replaced by real content in the near future. The purpose of this random content is to simply test and see how much space it takes up on the browser. Let us now see whether or not this test is successful. I sure hope it is considering how much random nonsense I am typing here.</p>
-                    </div>
+                    
                 </div>
             </div>
         </div>
