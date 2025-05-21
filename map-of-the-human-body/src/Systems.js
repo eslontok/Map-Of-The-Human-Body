5d45bfec5a22a4cc8298c491bc880839a48fe515
eslontok <146041985+eslontok@users.noreply.github.com>
2025-05-21 19:42:49 -0400

    Initialize Systems Page

--- map-of-the-human-body/src/Systems.js
+++ map-of-the-human-body/src/Systems.js
@@ -1,12 +1,27 @@
 import "./css/systems.css";
+import Observer from "./Observer";
 import ScrollToTop from "./ScrollToTop";
 
 function Systems(){
     return(
-        <div className="systems">
-            <ScrollToTop/>
-            <h2>Systems Page</h2>
-            <p>Coming Soon!</p>
+        <div className="systemsScene">
+            <div className="systemsList">
+                <button>Nervous</button>
+                <button>Endocrine</button>
+                <button>Musculoskeletal</button>
+                <button>Cardiovascular</button>
+                <button>Respiratory</button>
+                <button>Renal</button>
+                <button>Digestive</button>
+                <button>Reproductive</button>
+                <button>Immune</button>
+            </div>
+            <div className="systems">
+                <Observer/>
+                <ScrollToTop/>
+                <h2>Systems Page</h2>
+                <p>Coming Soon!</p>
+            </div>
         </div>
     );
 }
