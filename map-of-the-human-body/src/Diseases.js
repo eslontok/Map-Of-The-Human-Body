2ba3fa2cfbf3f447f5b5014ee8b5eabdfcebe3da
eslontok <146041985+eslontok@users.noreply.github.com>
2025-06-02 23:30:29 -0400

    Initialize Diseases Page

--- map-of-the-human-body/src/Diseases.js
+++ map-of-the-human-body/src/Diseases.js
@@ -1,12 +1,30 @@
 import "./css/diseases.css";
+import Observer from "./Observer";
 import ScrollToTop from "./ScrollToTop";
+import diseasesInfo from "./DiseasesInfo";
 
 function Diseases(){
     return(
-        <div className="diseases">
-            <ScrollToTop/>
-            <h2>Diseases Page</h2>
-            <p>Coming Soon!</p>
+        <div className="diseasesScene">
+            <div className="diseasesList">
+                <button>Heart Disease</button>
+                <button>Respiratory Disease</button>
+                <button>Kidney Disease</button>
+                <button>Influenza (Flu)</button>
+                <button>Diabetes</button>
+                <button>Cancer</button>
+            </div>
+            <div className="diseases">
+                <Observer/>
+                <ScrollToTop/>
+                {diseasesInfo.map((disease) => (
+                    <div className="diseaseDetails" key={disease.title}>
+                        <h2 className="hide slideInRight">{disease.title}</h2>
+                        <img className="hide slideInRight animate-stagger-short" src={disease.image} alt=""></img>
+                        <p className="hide slideInRight animate-stagger-long" style={{whiteSpace: "pre-line"}}>{disease.body}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
