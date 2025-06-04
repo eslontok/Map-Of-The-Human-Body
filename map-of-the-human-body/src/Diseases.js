4031e4ed4e8a7e57ab1e552940a3dd369dfc5a2d
eslontok <146041985+eslontok@users.noreply.github.com>
2025-06-04 02:15:47 -0400

    Add Scroll To Disease

--- map-of-the-human-body/src/Diseases.js
+++ map-of-the-human-body/src/Diseases.js
@@ -1,24 +1,44 @@
 import "./css/diseases.css";
+import {useRef} from "react";
 import Observer from "./Observer";
 import ScrollToTop from "./ScrollToTop";
 import diseasesInfo from "./DiseasesInfo";
 
 function Diseases(){
+
+    const refs = useRef();
+
+    const scrollToDisease = (title) => {
+        const map = initializeMap();
+        const node = map.get(title);
+        node.scrollIntoView({behavior: "smooth"});
+    }
+
+    const initializeMap = () => {
+        if(refs.current == null){
+            refs.current = new Map();
+        }
+        return refs.current;
+    }
+
     return(
         <div className="diseasesScene">
             <div className="diseasesList">
-                <button>Heart Disease</button>
-                <button>Respiratory Disease</button>
-                <button>Kidney Disease</button>
-                <button>Influenza (Flu)</button>
-                <button>Diabetes</button>
-                <button>Cancer</button>
+                <button onClick={() => scrollToDisease("Heart Disease")}>Heart Disease</button>
+                <button onClick={() => scrollToDisease("Respiratory Disease")}>Respiratory Disease</button>
+                <button onClick={() => scrollToDisease("Kidney Disease")}>Kidney Disease</button>
+                <button onClick={() => scrollToDisease("Influenza (Flu)")}>Influenza (Flu)</button>
+                <button onClick={() => scrollToDisease("Diabetes")}>Diabetes</button>
+                <button onClick={() => scrollToDisease("Cancer")}>Cancer</button>
             </div>
             <div className="diseases">
                 <Observer/>
                 <ScrollToTop/>
                 {diseasesInfo.map((disease) => (
-                    <div className="diseaseDetails" key={disease.title}>
+                    <div className="diseaseDetails" key={disease.title} ref={(node) => {
+                        const map = initializeMap();
+                        map.set(disease.title, node);
+                    }}>
                         <h2 className="hide slideInRight">{disease.title}</h2>
                         <img className="hide slideInRight animate-stagger-short" src={disease.image} alt=""></img>
                         <p className="hide slideInRight animate-stagger-long" style={{whiteSpace: "pre-line"}}>{disease.body}</p>
